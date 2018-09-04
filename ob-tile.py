d84fb6ce7196e9631e5616207812a192804f6fc6
capn-damo <damo.linux@gmail.com>
2018-09-04 15:20:39 +0100

    Add sys.args

--- ob-tile.py
+++ ob-tile.py
@@ -1,21 +1,49 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+## apt-get install pip3 gir1.2-wnck-3.0 
+## pip3 install:   pynput screeninfo
 
-import gi, os, re, tempfile, pickle
+import gi, os, sys, re, tempfile, pickle
 gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 gi.require_version('Wnck', '3.0')
-from gi.repository import  Gtk, Wnck
+from gi.repository import  Wnck
 from screeninfo import get_monitors
 
+USAGE=('    ob-tile.py [arg]\n\n'+
+    '   -G |--grid      Tile up to 4 quartered windows; a fifth window will be centered;\
+                    Any others will be left alone.\n\n'+
+    '   -V |--vert      Tile up to 3 windows side by side; a 4th window will be centered;\n\
+                    Any others will be left alone.\n\n'+
+    '   -H |--horiz     Tile up to 3 windows above and below; a 4th window will be centered;\n\
+                    Any others will be left alone.\n\n'+
+    '   -M | --max      Toggle maximize all windows on desktop.\n\n'+
+    '       *           This USAGE.\n\n'+
+    '   With no script arguments the windows will be grid tiled, and original positions are not stored.'
+)
 
-screen = Wnck.Screen.get_default()
-screen.force_update()
-w = screen.get_width()
-h = screen.get_height()
 
-
+def cmd_args():
+    print('Num args= ',len(sys.argv))
+    if len(sys.argv) == 1:
+        return 0
+    else:
+        arg = sys.argv[1]
+        tiling = {
+                '-G':0,
+                '--grid':0,
+                '-V':1,
+                '--vert':1,
+                '-H':2,
+                '--horiz':2,
+                '-V3':3,
+                '--vert3':3,
+                '-H3':4,
+                '--horiz3':4,
+                }
+        return tiling.get(arg,'ERROR')
+    
 def dict_load_KBINDS():
     """ Dictionaries to hold keybinds previously set in rc.xml """
     GRID = {'TL':'super+alt+7','TR':'super+alt+9','BR':'super+alt+3','BL':'super+alt+1','C':'super+alt+5'}    # TL,TR,BR,BL,center
@@ -28,36 +56,44 @@ def dict_load_KBINDS():
 
     return KB
 
-def set_filepaths():
-    f_TMP_WIN_LIST = tempfile.NamedTemporaryFile(mode='w+b',prefix='tmp')   # stores window ID's
-    f_TMP_WIN_DIMS = os.environ['HOME'] + 'temp-windims.tmp'           # stores geometry for restoring windows
-    f_STORE_ARG = os.environ['HOME'] + 'ob_tiling.tmp'
-    f_STORE_WIN_CMDS = os.environ['HOME'] + 'win_ids.tmp'
+def set_filepaths(arg):
+    # f_TMP_WIN_LIST = tempfile.NamedTemporaryFile(mode='w+b',prefix='tmp')   # stores window ID's
+    # f_TMP_WIN_DIMS = os.environ['HOME'] + '/temp-windims.tmp'           # stores geometry for restoring windows
+    # f_STORE_ARG = os.environ['HOME'] + '/ob_tiling.tmp'
+    # f_STORE_WIN_CMDS = os.environ['HOME'] + '/win_ids.tmp'
+    #return f_TMP_WIN_DIMS
+    
+    file_case = {
+                'store':os.environ['HOME'] + '/temp-windims.tmp',
+                'args':os.environ['HOME'] + '/ob_tiling.tmp',
+                'commands':os.environ['HOME'] + '/win_ids.tmp',
+                }
+    return file_case.get(arg,'Error')
+    
 
 def get_desktop_geometry():
 
     workspaces = screen.get_workspaces()
 
     desktop = Wnck.Screen.get_active_workspace(screen)
-    print('ActiveDesktop: ', desktop)
+    #print('ActiveDesktop: ', desktop)
     win_list = screen.get_windows()
 
     mon_LIST=[]
-    #mon_LIST.sort(key=int)
-
-    #for m in mon_LIST:
-        #print(m)
-
     for m in get_monitors():
         edge = re.findall('\+([^]]*)\+',str(m))
+        #print('edge= ',edge)
         if int(edge[0]) > 0:
             screen_edge = edge[0]
             mon_LIST.append(edge[0])
-        print('m= ',str(m))
-    print ('Number of Monitors= ', str(len(mon_LIST)+1))
-    print('Right Monitor starts at ', str(screen_edge))
+        else:
+            screen_edge = 0
+        #print('m= ',str(m))
+        monitors = len(mon_LIST)+1
+    print ('Number of Monitors= ', str(monitors))
+    #print('Right Monitor starts at ', str(screen_edge))
 
-    return int(screen_edge)
+    return int(monitors),screen_edge
 
 def get_open_windows():
     win_LIST = []
@@ -74,7 +110,7 @@ def get_open_windows():
         geom=[]
     for win in win_LIST:
         geom.append(win.get_geometry())
-    print(geom)
+    #print(geom)
 
     return win_LIST,geom
 
@@ -88,14 +124,26 @@ def write_f_pickle(fname,argsLIST):
     with open(fname,'wb') as f:
         pickle.dump(argsLIST,f)
     f.close()
-
+    
+def write_data(fname,data):
+    f = open(fname,'w')
+    f.write(str(data))
+    f.close
+    
+    
 def read_f_pickle(fname):
     f = open(fname,'rb')
     data = pickle.load(f)
-    return data
     f.close()
-    os.remove(f)
+    #os.remove(f)
+    return data
 
+def read_data(fname):
+    f = open(fname,'r')
+    data = f.read()
+    f.close()
+    return data
+    
 def get_win_geometry():
     geom=[]
     windows,wingeom = get_open_windows()
@@ -109,22 +157,24 @@ def get_win_geometry():
     return geom
 
 def get_monitor_pos():
-    edge = get_desktop_geometry()
+    monitors,edge = get_desktop_geometry()
     windows = get_win_geometry()
     for w in windows:
-
-        # see if window is mainly on left or right monitor
-        # Openbox uses 33%?
-        win_right = (w[0]+w[2])
-        win_centre = ((edge-w[0])/w[2]*100)
-        #print('screen edge= ',edge,' Centre= ',win_centre,'%')
-        if (win_right <= edge):
-            if (win_centre > 33):
-                monitor = 1
+        if monitors > 1:
+            # see if window is mainly on left or right monitor
+            # Openbox uses 33%?
+            win_right = (w[0]+w[2])
+            win_centre = ((edge-w[0])/w[2]*100)
+            #print('screen edge= ',edge,' Centre= ',win_centre,'%')
+            if (win_right <= edge):
+                if (win_centre > 33):
+                    monitor = 1
+            else:
+                monitor = 2
         else:
-            monitor = 2
+            monitor = 1
 
-        #print('Window geometry: ',w[0], w[1], w[2], w[3],' on Monitor ',monitor)
+        print('Window geometry: ',w[0], w[1], w[2], w[3],' on Monitor ',monitor)
 
 def get_mouse_on_monitor():
     # xdotool getmouselocation
@@ -132,39 +182,71 @@ def get_mouse_on_monitor():
     mouse = Controller()
     print('Mouse x position= ',mouse.position[0])
 
+def store_window_data(fname):
+    """ get window positions on monitor(s), and write to file """
+    windows,wingeom = get_open_windows()
+    print('File= ',fname)
+    win_data = []
+    i = 0
+    while i < len(windows):
+        g = wingeom[i]
+        print('Win= ',windows[i].get_name(),g[0], g[1], g[2], g[3])
+        win_data.append([str(windows[i]),str(windows[i].get_name()),g[0], g[1], g[2], g[3]])
+        i += 1
+    #print(win_data)
+    write_f_pickle(fname,win_data)
+
+def get_window_data(fname):
+    if os.path.exists(fname):
+        dataLIST = read_f_pickle(fname)
+        print(dataLIST)
+    else:
+        print('no file found')
+        
+    
 if __name__ == "__main__":
 
+    screen = Wnck.Screen.get_default()
+    screen.force_update()
+    w = screen.get_width()
+    h = screen.get_height()
+	
+    command = cmd_args()
+    if command == 'ERROR':
+        print(USAGE)
+        sys.exit()
+    else:
+        print('Command= ',command)
+        keybinds = dict_load_KBINDS()
+        print(keybinds[command])        # use list index from cmd_args
+
     #print('edge= ',get_desktop_geometry())
-    #windows,wingeom = get_open_windows()
+    # windows,wingeom = get_open_windows()
 
-    #win_data = []
-    #i = 0
-    #while i < len(windows):
-        #g = wingeom[i]
-        #print('Win= ',windows[i].get_name(),g[0], g[1], g[2], g[3])
-        #win_data.append([windows[i],windows[i].get_name(),g[0], g[1], g[2], g[3]])
-        #i += 1
+    # win_data = []
+    # i = 0
+    # while i < len(windows):
+        # g = wingeom[i]
+        # print('Win= ',windows[i].get_name(),g[0], g[1], g[2], g[3])
+        # win_data.append([str(windows[i]),str(windows[i].get_name()),g[0], g[1], g[2], g[3]])
+        # i += 1
+    #print(win_data)
+    #fname = set_filepaths()
+    
+    # fname = set_filepaths('store')      # options are 'store','args','commands'
+    # if fname == 'Error':
+        # print('Filename not set properly\nExiting...')
+        # sys.exit()
+    # else:
+        # print('Filename= ',fname)   
+        # store_window_data(fname)
+        # print('------------')
+        # get_window_data(fname)
 
-    #fpath = os.path.expanduser('~/tmp')
-    #fname = os.path.join(fpath,'windata.txt')
-    #write_f_pickle(fname,win_data)
-
-    #print('------------')
-    #if os.path.exists(fname):
-        #dataLIST = read_f_pickle(fname)
-        #for item in dataLIST:
-            #print(item[2])
-    #else:
-        #print('no file found')
-
-    #if os.path.exists(fname):
-        #print('file exists')
-    #else:
-        #print('file has gone')
 
     #grid,vert,horiz,vert3,horiz3 = dict_load_KBINDS()
     #print('grid: ',grid.keys())
     #print(grid['TL'],vert['L'])
 
     #get_monitor_pos()
-    get_mouse_on_monitor()
+    #get_mouse_on_monitor()
